60746fb295e7494e762b43e1a17ff789bab67ca1
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-12 23:12:21 +0500

    +problem3

---
+++ Problem3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem3 {
+    class Program {
+        static void Main(string[] args) {
+            double S = 1000;
+            double P = Read("P");
+            
+            int K;
+
+            for(K = 0; S < 1100; K++) {
+                S += S * P / 100;
+            }
+
+            System.Console.WriteLine($"K = {K}");
+            System.Console.WriteLine($"S = {S}");
+        }
+
+        static double Read(string varName) {
+            Console.Write($"{varName} = ");
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
