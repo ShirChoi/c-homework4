603c3fbfc9f4bce520a93b128fd2a85c29f01ac3
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-12 22:33:03 +0500

    +Problem1

---
+++ Problem1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            int A = Read("A");
+            int B = Read("B");
+
+            int sum = 0;
+
+            for(int i = A + 1; i < B; i++) 
+                sum += i;
+
+            System.Console.WriteLine($"sum = {sum}");
+
+            for(int i = A + 1; i < B; i++)
+                if(i % 2 == 1)
+                    System.Console.Write($"{i} ");
+        }
+
+        static int Read(string varName) {
+            Console.Write($"{varName} = ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
